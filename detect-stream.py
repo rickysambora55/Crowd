a9a1b5e9b7a7140bd961025c76e77d94b0e14bdc
Ricky Sambora <rickysambora55@gmail.com>
2023-12-05 22:22:55 +0700

    Update detect-stream.py

--- detect-stream.py
+++ detect-stream.py
@@ -78,9 +78,12 @@ def run(model: str, camera_id: str, enable_edgetpu: bool) -> None:
 
         # Show the text
         fps_text = 'FPS = {:.1f}'.format(fps)
-        count_text = 'Count = ' + num_detections
+        count_text = 'Count = ' + str(num_detections)
         text_location = (left_margin, row_size)
+        text_location2 = (left_margin, row_size+10)
         cv2.putText(image, fps_text, text_location, cv2.FONT_HERSHEY_PLAIN,
+                    font_size, text_color, font_thickness)
+        cv2.putText(image, count_text, text_location2, cv2.FONT_HERSHEY_PLAIN,
                     font_size, text_color, font_thickness)
 
         # Stop the program if the ESC key is pressed.
